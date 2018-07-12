da0d33c64e5a5d42b24c5c2b219dba8e967f4bfc
pruthvimz <pruthvi.zandawala@gmail.com>
2018-07-12 21:51:08 +0530

    Vidboard frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Vidboard from "./Vidboard.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Vidboard/>
+      </div>
+    );
+  }
+}
+
+export default App;
