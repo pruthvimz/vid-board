36f81713330104d1b7d441af8306e14d55c15960
pruthvimz <pruthvi.zandawala@gmail.com>
2018-07-15 16:27:21 +0530

    url condition changed

--- src/container/MainSignInUp.js
+++ src/container/MainSignInUp.js
@@ -47,7 +47,7 @@ class MainSignInUp extends React.Component {
     let backendHost;
 
     console.log("hostname : "+hostname)
-    if(hostname === 'https://vid-board.herokuapp.com') {
+    if(hostname.indexOf('vid-board.herokuapp.com') > 0) {
       backendHost = 'https://vid-board-backend.herokuapp.com';
     } else {
       backendHost = process.env.REACT_APP_LOCAL_BACKEND || 'http://localhost:8080';
